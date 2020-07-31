6716a2a6671bc05029e002ae57e9bb753e25b9c0
MrBusiness87 <bbartula87@gmail.com>
2020-07-31 08:30:29 -0400

    07/31/2020-I have gotten the site to load on the local host and change over to the note taker side, but I'm troubleshooting my code to see why nothing saves or deletes yet.

---
+++ apiRoutes.js
@@ -0,0 +1,44 @@
+const fs = require("fs")
+const path = require("path");
+
+var noteData;
+
+module.exports = function (app) {
+  fs.readFile("./db/db.json", "utf8", function (err, data) {
+    if (err) throw err;
+    noteData = JSON.parse(data);
+  })
+
+  app.get("/api/notes", function (req, res) {
+    res.json(noteData);
+  });
+
+
+  app.post("/api/notes", function (req, res) {
+    console.log("test");
+    var newNote = req.body;
+    noteData.push(newNote);
+    let parsedata = JSON.stringify(noteData)
+    fs.writeFile(path.join('./db/db.json'), parsedata, (err) => {
+      if (err) throw err;
+    })
+    res.json(noteData);
+  });
+
+  app.delete("/api/notes/:id", function (req, res) {
+    console.log("erase");
+    var deleteData = req.params.id;
+    console.log(deleteData)
+    for (i = 0; i < noteData.length; i++) {
+      if (deleteData === noteData[i].title) {
+        noteData.splice(i, 1)
+      };
+    };
+    let parsedata = JSON.stringify(noteData)
+    fs.writeFile(path.join('./db/db.json'), parsedata, (err) => {
+      if (err) throw err;
+    })
+    console.log(noteData)
+    res.json(noteData)
+  })
+}
