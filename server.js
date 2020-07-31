ec7a3e09f9daa2f8146a0ae63b5417dc646409d4
MrBusiness87 <bbartula87@gmail.com>
2020-07-31 08:42:33 -0400

    07/31/2020-code is now saving file, but having trouble erasing file, working on that next.

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
-// Dependencies
 const express = require("express");
 
-// Setting up Express App
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({
   extended: true
 }));
 app.use(express.json());
 app.use(express.static('public'));
 
-//Creating Routes
 require("./apiRoutes")(app);
 require("./htmlRoute")(app);
 
-// Starts the server to begin listening
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
